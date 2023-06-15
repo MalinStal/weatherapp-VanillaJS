28c6550a8ede38756c1c80c52c6b4772fe773efc
MalinStal <malinstal.94@gmail.com>
2023-06-15 09:30:02 +0200

    conected time to uppdate whit timezone

--- weatherapp.js
+++ weatherapp.js
@@ -5,9 +5,9 @@ const mainInfo = document.getElementsByClassName("mainInformation")
 const showCity = document.getElementById("city");
 
 let api = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&lang=se&appid=3ee1fe60512466d911afa2c70e2190c9"
+let timeInterval = null;
 
-placeholder()
-setTimeAndDate();
+fetchPlaceholder();
 
 // event Listener
 searchBtn.addEventListener("click", e => {
@@ -26,27 +26,29 @@ setInterval(() => {
 ///////////////funktioner///////////////
 
 //funktion för tid och dag
-function setTimeAndDate() {
+function setTimeAndDate(timezone) {
     //pilfunktion för att sätta tiden. OBS!!! anpassar sig inte efter tidszoner
     let ShowDateTime = document.getElementById("dateTime")
-    setInterval(() => {
-        let currentTime = new Date().getTime();
-        let time = new Date(currentTime).toLocaleTimeString();
+    if (timeInterval !== null) {
+        clearInterval(timeInterval);
+    }
+
+    timeInterval = setInterval(() => {
         // let minut = nu.getMinutes().toString();
         //uppdelning för att skriva ut datum i rätt ordning
-        let showDate = new Date();
+        let showDate = calculateTime(120, timezone, new Date());
         let day = showDate.getDate().toString().padStart(2, "0");
         let month = showDate.getMonth() + 1
         let month2 = month.toString().padStart(2, "0");
         let year = showDate.getFullYear();
         let currentDate = `${day}-${month2}-${year}`;
-        ShowDateTime.innerHTML = "Datum: " + currentDate + ", Tid: " + time;
+        ShowDateTime.innerHTML = "Datum: " + currentDate + ", Tid: " + showDate.toLocaleTimeString();
     }, 1000)
 }
 
 //skriver ut vädret i göteborg tills ny sökning görs
-function placeholder() {
-   fetch("https://api.openweathermap.org/data/2.5/weather?q=göteborg&units=metric&lang=se&appid=3ee1fe60512466d911afa2c70e2190c9")
+function fetchPlaceholder() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=göteborg&units=metric&lang=se&appid=3ee1fe60512466d911afa2c70e2190c9")
         .then((respons) => respons.json())
         .then((json) => displayWeather(json))
 }
@@ -73,6 +75,9 @@ function fetchWeather(city) {
 
 //funktion - vilken väder information som ska vissas
 function displayWeather(display) {
+    setTimeAndDate(display.timezone / 60);
+
+    console.log(display)
 
     showCity.innerText = display.name;
 
@@ -95,6 +100,17 @@ function displayWeather(display) {
     showPressure.innerText = "Lufttryck: " + display.main.pressure + " hPa ";
 
 }
+
+function calculateTime(myOffset, timeZoneOffset, date) {
+    const minInMillis = 60 * 1000;
+    const timestamp = date.getTime();
+
+    const diff = myOffset - timeZoneOffset;
+
+    let newTimestamp = timestamp - diff * minInMillis;
+    return new Date(newTimestamp);
+}
+
 
 
 
